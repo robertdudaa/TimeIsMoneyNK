99edb3f940183908609fcf78cf1a52f273baf7f0
Robert Duda <robertduda20@gmail.com>
2020-06-26 15:12:07 +0200

    initial commit

---
+++ src/main/java/de/robasse/timeismoney/TimeIsMoney.java
@@ -0,0 +1,14 @@
+package de.robasse.timeismoney;
+
+import cn.nukkit.plugin.PluginBase;
+
+public class TimeIsMoney extends PluginBase {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
